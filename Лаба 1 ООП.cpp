ee74a1e288a9daf07b6d7b63ab98d5cc58b8ac26
Alex Koshka <avkoshh@gmail.com>
2025-10-03 23:18:52 +0300

    second commit

--- Лаба 1 ООП.cpp
+++ Лаба 1 ООП.cpp
@@ -9,14 +9,31 @@ int safe_input_int(const std::string& prompt) {
         std::cin >> value;
 
         if (std::cin.fail()) {
-            std::cout << "Ошибка! Пожалуйста, введите целое число.\n";
+            std::cout << "Ошибка! Пожалуйста, введите целое число в пределах int.\n";
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
         }
         else {
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
             return value;
+            
         }
     }
 }
 
+int safe_input_positive_int(const std::string& prompt) {
+    int value;
+    while (true) {
+        value = safe_input_int(prompt);
+        if (value >= 0) {
+            return value;
+        }
+        else {
+            std::cout << "Ошибка! Число не может быть отрицательным.\n";
+        }
+    }
+}
 
 class enterprise 
 {
@@ -72,20 +89,20 @@ int main()
     std::cin >> dis;
     housing_office.Set_district(dis);
 
-    num = safe_input_int("\nВведите номер ЖЭС: ");
+    num = safe_input_positive_int("\nВведите номер ЖЭС: ");
     housing_office.Set_number(num);
 
     std::cout << "\nТак так так. О мы нашли эту контору";
 
     
 
-    price = safe_input_int("\nКакая стоимость оплаты: ");
+    price = safe_input_positive_int("\nКакая стоимость оплаты: ");
     housing_office.Set_price(price);
 
     while (true) {
-        housing_office.number_people = safe_input_int("\nСколько сейчас людей в ней проживает: ");
+        housing_office.number_people = safe_input_positive_int("\nСколько сейчас людей в ней проживает: ");
 
-        housing_office.number_paid = safe_input_int("\nА сколько из них всё оплатили: ");
+        housing_office.number_paid = safe_input_positive_int("\nА сколько из них всё оплатили: ");
         if (housing_office.number_people >= housing_office.number_paid) {
             break;
         }
